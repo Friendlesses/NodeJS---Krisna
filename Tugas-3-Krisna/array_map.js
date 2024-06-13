08a844006d63feaa98e5b765999ef80b667f7d61
Friendlesses <99161473+Friendlesses@users.noreply.github.com>
2024-06-13 08:38:43 +0700

    array_map.js

--- Tugas-3-Krisna/array_map.js
+++ Tugas-3-Krisna/array_map.js
@@ -1,3 +1,4 @@
 let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 let doubled = numbers.map(num => num * 2);
-console.log(doubled); 
+console.log('Nilai awal = ', numbers)
+console.log('Nilai setelah dikali 2 = ', doubled); 

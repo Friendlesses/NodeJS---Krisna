af5e34d489c758bd5f3fa26a8ee9d5d6fed02c36
Friendlesses <krisumahendra@gmail.com>
2024-06-15 04:55:16 +0700

    Tugas-5-Krisna

---
+++ Tugas-5-Krisna/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at <http://localhost>:${port}`);
+});
+
+
---
+++ Tugas-5-Krisna/kelola_route.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.get('/hello', (req, res) => {
+    res.json({
+        message: "Success fetch message",
+        data: "Hello World!"
+    });
+});
+
+app.get('/user', (req, res) => {
+    res.json({
+        message: "Success fetch user",
+        data: {
+            id: 1,
+            name: "Budi",
+            username: "budidu",
+            email: "budidu@mail.com"
+        }
+    });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server is running at <http://localhost>:${port}`);
+});

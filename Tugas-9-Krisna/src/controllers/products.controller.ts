52eda3d32a484df1a4e49015fe34cb060ff355a8
Friendlesses <99161473+Friendlesses@users.noreply.github.com>
2024-06-21 18:22:47 +0700

    Update products.controller.ts
    Menambahkan populate

--- Tugas-9-Krisna/src/controllers/products.controller.ts
+++ Tugas-9-Krisna/src/controllers/products.controller.ts
@@ -19,7 +19,7 @@ export default {
   },
   async findAll(req: Request, res: Response) {
     try {
-      const result = await ProductsModel.find();
+      const result = await ProductsModel.find().populate('category');
       res.status(200).json({
         data: result,
         message: "Success get all products",
@@ -36,7 +36,7 @@ export default {
     try {
       const result = await ProductsModel.findOne({
         _id: req.params.id,
-      });
+      }).populate('category');
       res.status(200).json({
         data: result,
         message: "Success get one product",
@@ -57,7 +57,7 @@ export default {
         {
           new: true,
         }
-      );
+      ).populate('category');
 
       res.status(200).json({
         data: result,
@@ -75,7 +75,7 @@ export default {
     try {
       const result = await ProductsModel.findOneAndDelete({
         _id: req.params.id,
-      });
+      }).populate('category');
 
       res.status(200).json({
         data: result,
